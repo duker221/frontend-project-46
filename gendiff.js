a55a703697275c5c62019bf9a2413bdc5bfb1960
duker22 <dukin.misha22@gmail.com>
2023-12-01 18:01:45 +0300

    update gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -4,26 +4,28 @@ import { Command } from "commander";
 
 const program = new Command();
 
+
 program
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0');
-
 program
     .option('-h, --help', 'output usage information');
-
 program
-    .option('-f --format <type>', 'output format')
+    .option('-f --format <type>', 'output format');
+program 
+    .usage('[options] <filepath1> <filepath2>')
 
 program.parse(process.argv);
 
 const options = program.opts();
 
-if (options.version) {
-    console.log(program.version());
-} else if (options.help) {
+if (!process.argv.slice(2).length) {
     program.outputHelp();
 }
 
+if (options.version) {
+    console.log(program.version());
+}
+if (options.help) {
+    program.outputHelp();
+} 
