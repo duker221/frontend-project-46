b83e258f5d2406bcf1c942f2b6e24ece56536f3f
duker22 <dukin.misha22@gmail.com>
2023-12-08 03:14:24 +0300

    update eslint rules

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -6,12 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 test('Compares two configuration files and shows a difference', () => {
-    const obj1 = path.join(__dirname, '_fixtures_', 'file1.json');
-    const obj2 = path.join(__dirname, '_fixtures_', 'file2.json');
+  const obj1 = path.join(__dirname, '_fixtures_', 'file1.json');
+  const obj2 = path.join(__dirname, '_fixtures_', 'file2.json');
 
-    const expectedDiff = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
+  const expectedDiff = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
 
-    const actualDiff = genDiff(obj1, obj2);
+  const actualDiff = genDiff(obj1, obj2);
 
-    expect(actualDiff).toEqual(expectedDiff);
+  expect(actualDiff).toEqual(expectedDiff);
 });
