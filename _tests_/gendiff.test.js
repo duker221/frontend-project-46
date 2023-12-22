7237082a37dca5f0653f77bf1a6082327e75482d
duker22 <dukin.misha22@gmail.com>
2023-12-22 20:30:25 +0300

    TEST

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -3,6 +3,7 @@ import * as path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 import parseFile from '../src/parsers.js';
+import { stylishTree } from '../src/formatters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,15 +15,15 @@ describe('genDiff', () => {
   test.each([
     ['file1.json', 'file2.json', 'stylish', 'stylish_result.txt'],
     ['file1.json', 'file2.json', 'plain', 'plain_result.txt'],
-    ['file1.yaml', 'file2.yaml', 'stylish', 'stylish_result.txt'],
-    ['file1.yaml', 'file2.yaml', 'plain', 'plain_result.txt'],
+    ['file1.yml', 'file2.yml', 'stylish', 'stylish_result.txt'],
+    ['file1.yml', 'file2.yml', 'plain', 'plain_result.txt'],
     ['file1.json', 'file2.json', 'json', 'json_result.json'],
-    ['file1.yaml', 'file2.yaml', 'json', 'json_result.json'],
+    ['file1.yml', 'file2.yml', 'json', 'json_result.json'],
   ])('should generate diff with %s, %s and format %s', (file1, file2, format, expectedResultFile) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
     const expectedResult = getExpectedResult(expectedResultFile);
 
-    expect(genDiff(filepath1, filepath2, format, parseFile)).toEqual(expectedResult);
+    expect(genDiff(filepath1, filepath2, format, parseFile, stylishTree)).toEqual(expectedResult);
   });
 });
