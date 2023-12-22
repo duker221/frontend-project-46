48b81887d44a3efc2b53b9b9c4b0ac07caa2515d
duker22 <dukin.misha22@gmail.com>
2023-12-22 21:26:15 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import stylishTreeFormat from './formatters/stylish.js';
 import buildDiff from './buildDifferenceTree.js';
 import getFormatter from './formatters/format.js';
 
-const genDiff = (filePath1, filePath2, formatter = stylishTreeFormat) => {
+const genDiff = (filePath1, filePath2, formatter = 'stylish') => {
   const file1Data = parseFile(filePath1);
   const file2Data = parseFile(filePath2);
 
